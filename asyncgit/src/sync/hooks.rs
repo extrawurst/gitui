bedf18f173521dba1786ef004bef6d09cc70a466
Stephan Dilly <dilly.stephan@gmail.com>
2020-04-09 00:21:13 +0200

    some unittests

--- asyncgit/src/sync/hooks.rs
+++ asyncgit/src/sync/hooks.rs
@@ -24,9 +24,13 @@ pub fn hooks_commit_msg(
 
         let file_path = file.path().to_str().unwrap();
 
+        dbg!(&file_path);
+
         let res = run_hook(repo_path, HOOK_COMMIT_MSG, &[&file_path]);
 
         if let HookResult::NotOk(e) = res {
+            let mut file = file.reopen().unwrap();
+            msg.clear();
             file.read_to_string(msg).unwrap();
             HookResult::NotOk(e)
         } else {
@@ -56,6 +60,7 @@ fn hook_runable(path: &str, hook: &str) -> bool {
 }
 
 ///
+#[derive(Debug, PartialEq)]
 pub enum HookResult {
     /// Everything went fine
     Ok,
@@ -77,5 +82,93 @@ fn run_hook(path: &str, cmd: &str, args: &[&str]) -> HookResult {
         let formatted = format!("{}{}", out, err);
 
         HookResult::NotOk(formatted)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::sync::tests::repo_init;
+    use std::fs::File;
+
+    #[test]
+    fn test_smoke() {
+        let (_td, repo) = repo_init();
+        let root = repo.path().parent().unwrap();
+        let repo_path = root.as_os_str().to_str().unwrap();
+
+        let mut msg = String::from("test");
+        let res = hooks_commit_msg(repo_path, &mut msg);
+
+        assert_eq!(res, HookResult::Ok);
+
+        let res = hooks_post_commit(repo_path);
+
+        assert_eq!(res, HookResult::Ok);
+    }
+
+    #[test]
+    fn test_hooks_commit_msg_ok() {
+        let (_td, repo) = repo_init();
+        let root = repo.path().parent().unwrap();
+        let repo_path = root.as_os_str().to_str().unwrap();
+
+        let hook = b"
+#!/bin/sh
+exit 0
+        ";
+
+        File::create(&root.join(HOOK_COMMIT_MSG))
+            .unwrap()
+            .write_all(hook)
+            .unwrap();
+
+        Command::new("chmod")
+            .args(&["+x", HOOK_COMMIT_MSG])
+            .current_dir(root)
+            .output()
+            .unwrap();
+
+        let mut msg = String::from("test");
+        let res = hooks_commit_msg(repo_path, &mut msg);
+
+        assert_eq!(res, HookResult::Ok);
+
+        assert_eq!(msg, String::from("test"));
+    }
+
+    #[test]
+    fn test_hooks_commit_msg() {
+        let (_td, repo) = repo_init();
+        let root = repo.path().parent().unwrap();
+        let repo_path = root.as_os_str().to_str().unwrap();
+
+        let hook = b"
+#!/bin/sh
+echo 'msg' > $1
+echo 'rejected'
+exit 1
+        ";
+
+        File::create(&root.join(HOOK_COMMIT_MSG))
+            .unwrap()
+            .write_all(hook)
+            .unwrap();
+
+        Command::new("chmod")
+            .args(&["+x", HOOK_COMMIT_MSG])
+            .current_dir(root)
+            .output()
+            .unwrap();
+
+        let mut msg = String::from("test");
+        let res = hooks_commit_msg(repo_path, &mut msg);
+
+        assert_eq!(
+            res,
+            HookResult::NotOk(String::from("rejected\n"))
+        );
+
+        assert_eq!(msg, String::from("msg\n"));
     }
 }
