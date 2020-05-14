1076c26c135172a21ce97d041b860ef7ceb172e2
Mehran KORDI <mkordi@gmail.com>
2020-05-14 14:18:43 +0200

    adding a custom error type.

---
+++ asyncgit/src/error.rs
@@ -0,0 +1,22 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("`{0}`")]
+    Generic(String),
+
+    #[error("io error")]
+    Io {
+        #[from]
+        source: std::io::Error,
+    },
+
+    #[error("git error")]
+    Git {
+        #[from]
+        source: git2::Error,
+    },
+
+    #[error(transparent)]
+    Other(#[from] Box<dyn std::error::Error>),
+}
