19abcf861860d1a417c9ff44c3bc2754a9104e42
Stephan Dilly <dilly.stephan@gmail.com>
2020-08-27 22:54:37 +0200

    more nightly clippy fixesds

--- src/components/filetree.rs
+++ src/components/filetree.rs
@@ -113,14 +113,12 @@ impl FileTreeComponent {
 
     ///
     pub fn is_file_seleted(&self) -> bool {
-        if let Some(item) = self.tree.selected_item() {
+        self.tree.selected_item().map_or(false, |item| {
             match item.kind {
                 FileTreeItemKind::File(_) => true,
                 FileTreeItemKind::Path(..) => false,
             }
-        } else {
-            false
-        }
+        })
     }
 
     fn move_selection(&mut self, dir: MoveSelection) -> bool {
