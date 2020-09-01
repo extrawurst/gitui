f0d3e29283dff0ec052e90eea713362e67358446
Stephan Dilly <dilly.stephan@gmail.com>
2020-09-01 23:49:19 +0200

    add first remotes test

---
+++ asyncgit/src/sync/remotes.rs
@@ -0,0 +1,84 @@
+//!
+
+use crate::{error::Result, sync::utils};
+use git2::{Cred, FetchOptions, RemoteCallbacks};
+use scopetime::scope_time;
+
+///
+pub fn get_remotes(repo_path: &str) -> Result<Vec<String>> {
+    scope_time!("get_remotes");
+
+    let repo = utils::repo(repo_path)?;
+    let remotes = repo.remotes()?;
+    let remotes: Vec<String> =
+        remotes.iter().filter_map(|s| s).map(String::from).collect();
+
+    Ok(remotes)
+}
+
+///
+pub fn remote_push_master(repo_path: &str) -> Result<()> {
+    scope_time!("remote_push_master");
+
+    let repo = utils::repo(repo_path)?;
+    let mut remote = repo.find_remote("origin")?;
+
+    remote.push(&["refs/heads/master"], None)?;
+
+    Ok(())
+}
+
+///
+pub fn remote_fetch_master(repo_path: &str) -> Result<usize> {
+    scope_time!("remote_fetch_master");
+
+    let repo = utils::repo(repo_path)?;
+    let mut remote = repo.find_remote("origin")?;
+
+    let mut callbacks = RemoteCallbacks::new();
+    callbacks.credentials(|url, username_from_url, allowed_types| {
+        log::debug!(
+            "creds: '{}' {:?} ({:?})",
+            url,
+            username_from_url,
+            allowed_types
+        );
+
+        Cred::ssh_key_from_agent(
+            username_from_url.expect("username not found"),
+        )
+    });
+
+    let mut options = FetchOptions::new();
+    options.remote_callbacks(callbacks);
+
+    remote.fetch(&["master"], Some(&mut options), None)?;
+
+    Ok(remote.stats().received_bytes())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::sync::tests::debug_cmd_print;
+    use tempfile::TempDir;
+
+    #[test]
+    fn test_smoke() {
+        let td = TempDir::new().unwrap();
+
+        debug_cmd_print(
+            td.path().as_os_str().to_str().unwrap(),
+            "git clone https://github.com/extrawurst/brewdump.git",
+        );
+
+        let repo_path = td.path().join("brewdump");
+        let repo_path = repo_path.as_os_str().to_str().unwrap();
+
+        let remotes = get_remotes(repo_path).unwrap();
+
+        assert_eq!(remotes, vec![String::from("origin")]);
+
+        remote_fetch_master(repo_path).unwrap();
+    }
+}
