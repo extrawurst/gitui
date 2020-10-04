7d292ca4f923ec4f7e320f0bc780356c166bee1d
Eduardo Flores <edfloreshz@gmail.com>
2020-10-04 13:05:20 -0700

    Handled crash when git2 failed to obtain username from URL

--- asyncgit/src/sync/remotes.rs
+++ asyncgit/src/sync/remotes.rs
@@ -4,12 +4,10 @@ use crate::{error::Result, sync::utils};
 use crossbeam_channel::Sender;
 use git2::{
     Cred, FetchOptions, PackBuilderStage, PushOptions,
-    RemoteCallbacks,
+    RemoteCallbacks, Error as GitError
 };
 use scopetime::scope_time;
-
 use super::CommitId;
-
 ///
 #[derive(Debug, Clone)]
 pub enum ProgressNotification {
@@ -58,7 +56,7 @@ pub fn get_remotes(repo_path: &str) -> Result<Vec<String>> {
     let repo = utils::repo(repo_path)?;
     let remotes = repo.remotes()?;
     let remotes: Vec<String> =
-        remotes.iter().filter_map(|s| s).map(String::from).collect();
+      remotes.iter().filter_map(|s| s).map(String::from).collect();
 
     Ok(remotes)
 }
@@ -71,7 +69,10 @@ pub fn fetch_origin(repo_path: &str, branch: &str) -> Result<usize> {
     let mut remote = repo.find_remote("origin")?;
 
     let mut options = FetchOptions::new();
-    options.remote_callbacks(remote_callbacks(None));
+    options.remote_callbacks(match remote_callbacks(None) {
+        Ok(callback) => callback,
+        Err(e) => return Err(e)
+    });
 
     remote.fetch(&[branch], Some(&mut options), None)?;
 
@@ -92,7 +93,10 @@ pub fn push(
 
     let mut options = PushOptions::new();
 
-    options.remote_callbacks(remote_callbacks(Some(progress_sender)));
+    options.remote_callbacks(match remote_callbacks(Some(progress_sender)) {
+        Ok(callbacks) => callbacks,
+        Err(e) => return Err(e)
+    });
     options.packbuilder_parallelism(0);
 
     remote.push(&[branch], Some(&mut options))?;
@@ -102,7 +106,7 @@ pub fn push(
 
 fn remote_callbacks<'a>(
     sender: Option<Sender<ProgressNotification>>,
-) -> RemoteCallbacks<'a> {
+) -> Result<RemoteCallbacks<'a>> {
     let mut callbacks = RemoteCallbacks::new();
     let sender_clone = sender.clone();
     callbacks.push_transfer_progress(move |current, total, bytes| {
@@ -167,12 +171,17 @@ fn remote_callbacks<'a>(
             allowed_types
         );
 
-        Cred::ssh_key_from_agent(
-            username_from_url.expect("username not found"),
-        )
+        match username_from_url {
+            Some(username) => {
+                Cred::ssh_key_from_agent(
+                    username,
+                )
+            },
+            None => Err(GitError::from_str(" Couldn't extract username from url."))
+        }
     });
 
-    callbacks
+    Ok(callbacks)
 }
 
 #[cfg(test)]
