0b97c545eda4eda450d670609d7f9cfbc0f9502e
Stephan Dilly <dilly.stephan@gmail.com>
2020-10-11 00:23:20 +0200

    cleanup clipboard and add to changelog (#325)

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -2,11 +2,10 @@ use anyhow::Result;
 use std::io::Write;
 use std::process::{Command, Stdio};
 
-fn execute_copy_command(
-    command: &mut Command,
-    string: &str,
-) -> Result<()> {
+fn execute_copy_command(command: Command, text: &str) -> Result<()> {
     use anyhow::anyhow;
+
+    let mut command = command;
 
     let mut process = command
         .stdin(Stdio::piped())
@@ -18,7 +17,7 @@ fn execute_copy_command(
         .stdin
         .as_mut()
         .ok_or_else(|| anyhow!("`{:?}`", command))?
-        .write_all(string.as_bytes())
+        .write_all(text.as_bytes())
         .map_err(|e| anyhow!("`{:?}`: {}", command, e))?;
 
     process
@@ -30,18 +29,17 @@ fn execute_copy_command(
 
 #[cfg(target_os = "linux")]
 pub fn copy_string(string: &str) -> Result<()> {
-    execute_copy_command(
-        Command::new("xclip").arg("-selection").arg("clipboard"),
-        string,
-    )
+    let mut cmd = Command::new("xclip");
+    cmd.arg("-selection").arg("clipboard");
+    execute_copy_command(cmd, string)
 }
 
 #[cfg(target_os = "macos")]
 pub fn copy_string(string: &str) -> Result<()> {
-    execute_copy_command(&mut Command::new("pbcopy"), string)
+    execute_copy_command(Command::new("pbcopy"), string)
 }
 
 #[cfg(windows)]
 pub fn copy_string(string: &str) -> Result<()> {
-    execute_copy_command(&mut Command::new("clip"), string)
+    execute_copy_command(Command::new("clip"), string)
 }
