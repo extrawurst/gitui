bc726aea09fa3374ef00be14840f8606c229fb21
Arnaud Locquet <alocquet@gmail.com>
2020-10-16 10:10:41 +0200

    feat: add branch selection popup in the log view

--- src/tabs/revlog.rs
+++ src/tabs/revlog.rs
@@ -236,6 +236,11 @@ impl Component for Revlog {
                         .borrow_mut()
                         .push_back(InternalEvent::CreateBranch);
                     return Ok(true);
+                } else if k == self.key_config.select_branch {
+                    self.queue
+                        .borrow_mut()
+                        .push_back(InternalEvent::SelectBranch);
+                    return Ok(true);
                 }
             }
         }
@@ -273,6 +278,14 @@ impl Component for Revlog {
 
         out.push(CommandInfo::new(
             strings::commands::open_branch_create_popup(
+                &self.key_config,
+            ),
+            true,
+            self.visible || force_all,
+        ));
+
+        out.push(CommandInfo::new(
+            strings::commands::open_branch_select_popup(
                 &self.key_config,
             ),
             true,
