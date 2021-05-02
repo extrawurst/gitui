0afcc9e0b6e7ca2482bc75ac838bbf1d294c3cf9
Anton Rapetov <willir29@gmail.com>
2021-05-01 22:24:59 -0400

    Reformat with rustfmt

--- asyncgit/src/sync/commits_info.rs
+++ asyncgit/src/sync/commits_info.rs
@@ -5,7 +5,9 @@ use scopetime::scope_time;
 use unicode_truncate::UnicodeTruncateStr;
 
 /// identifies a single commit
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Ord, PartialOrd)]
+#[derive(
+    Debug, Copy, Clone, PartialEq, Eq, Hash, Ord, PartialOrd,
+)]
 pub struct CommitId(Oid);
 
 impl CommitId {
