9117a1a5b711d1a73188d5652a8edb210bb1d665
Stephan Dilly <dilly.stephan@gmail.com>
2021-05-16 14:08:47 +0200

    no file contents
    changelog

--- src/bug_report.rs
+++ src/bug_report.rs
@@ -3,31 +3,23 @@ use bugreport::{
     bugreport,
     collector::{
         CommandLine, CompileTimeInformation, EnvironmentVariables,
-        FileContent, OperatingSystem, SoftwareVersion,
+        OperatingSystem, SoftwareVersion,
     },
     format::Markdown,
 };
 
-use crate::get_app_config_path;
-
 pub fn generate_bugreport() -> Result<()> {
-    let mut config_file = get_app_config_path()?;
-    config_file.push("gitui/");
-
     bugreport!()
         .info(SoftwareVersion::default())
         .info(OperatingSystem::default())
         .info(CompileTimeInformation::default())
-        .info(EnvironmentVariables::list(&["SHELL", "EDITOR"]))
+        .info(EnvironmentVariables::list(&[
+            "SHELL",
+            "EDITOR",
+            "GIT_EDITOR",
+            "VISUAL",
+        ]))
         .info(CommandLine::default())
-        .info(FileContent::new(
-            "theme.ron",
-            config_file.with_file_name("theme.ron"),
-        ))
-        .info(FileContent::new(
-            "key_config.ron",
-            config_file.with_file_name("key_config.ron"),
-        ))
         .print::<Markdown>();
     Ok(())
 }
