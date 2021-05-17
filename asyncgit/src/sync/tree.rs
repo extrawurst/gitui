8c079f107069bb430a0e31c71e18e5d349b21dac
Stephan Dilly <dilly.stephan@gmail.com>
2021-05-17 09:22:11 +0200

    allow reading tree file content

--- asyncgit/src/sync/tree.rs
+++ asyncgit/src/sync/tree.rs
@@ -1,18 +1,25 @@
 use super::utils::bytes2string;
 use crate::{error::Result, sync::utils::repo};
-use git2::{Repository, Tree};
+use git2::{Oid, Repository, Tree};
 use scopetime::scope_time;
 use std::path::{Path, PathBuf};
 
+pub struct TreeFile {
+    pub path: PathBuf,
+    pub filemode: i32,
+    id: Oid,
+}
+
 ///
-pub fn tree_files(repo_path: &str) -> Result<Vec<String>> {
+//TODO: allow any commit
+pub fn tree_files(repo_path: &str) -> Result<Vec<TreeFile>> {
     scope_time!("tree_files");
 
     let repo = repo(repo_path)?;
 
     let tree = repo.head()?.peel_to_tree()?;
 
-    let mut files: Vec<String> = Vec::new();
+    let mut files: Vec<TreeFile> = Vec::new();
 
     tree_recurse(&repo, &PathBuf::from("./"), &tree, &mut files)?;
 
@@ -20,11 +27,26 @@ pub fn tree_files(repo_path: &str) -> Result<Vec<String>> {
 }
 
 ///
+pub fn tree_file_content(
+    repo_path: &str,
+    file: &TreeFile,
+) -> Result<String> {
+    scope_time!("tree_file_content");
+
+    let repo = repo(repo_path)?;
+
+    let blob = repo.find_blob(file.id)?;
+    let content = String::from_utf8(blob.content().into())?;
+
+    Ok(content)
+}
+
+///
 fn tree_recurse(
     repo: &Repository,
     path: &Path,
     tree: &Tree,
-    out: &mut Vec<String>,
+    out: &mut Vec<TreeFile>,
 ) -> Result<()> {
     out.reserve(tree.len());
 
@@ -32,10 +54,9 @@ fn tree_recurse(
         let path = path.join(bytes2string(e.name_bytes())?);
         match e.kind() {
             Some(git2::ObjectType::Blob) => {
-                // log::info!("file: {:?}", path);
-                if let Some(n) = path.to_str() {
-                    out.push(n.to_string());
-                }
+                let id = e.id();
+                let filemode = e.filemode();
+                out.push(TreeFile { path, filemode, id });
             }
             Some(git2::ObjectType::Tree) => {
                 let obj = e.to_object(repo)?;
@@ -59,11 +80,17 @@ mod tests {
         let root = repo.path().parent().unwrap();
         let repo_path = root.as_os_str().to_str().unwrap();
 
-        write_commit_file(&repo, "test.txt", "", "c1");
+        write_commit_file(&repo, "test.txt", "content", "c1");
 
         let files = tree_files(repo_path).unwrap();
 
         assert_eq!(files.len(), 1);
-        assert_eq!(&files[0], "./test.txt");
+        assert_eq!(files[0].path, PathBuf::from("./test.txt"));
+
+        write_commit_file(&repo, "test.txt", "content2", "c2");
+
+        let content =
+            tree_file_content(repo_path, &files[0]).unwrap();
+        assert_eq!(&content, "content");
     }
 }
