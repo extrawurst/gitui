35bc78b259e5c6321b1c401cac780950d702fdf4
Alexandru Macovei <alexnmaco@gmail.com>
2022-10-09 13:38:17 +0300

    (cli) use derive support from clap to shorten builder and parsing code

--- src/args.rs
+++ src/args.rs
@@ -1,13 +1,9 @@
 use crate::bug_report;
 use anyhow::{anyhow, Result};
 use asyncgit::sync::RepoPath;
-use clap::{
-	crate_authors, crate_description, crate_name, crate_version, Arg,
-	Command as ClapApp,
-};
+use clap::Parser;
 use simplelog::{Config, LevelFilter, WriteLogger};
 use std::{
-	env,
 	fs::{self, File},
 	path::PathBuf,
 };
@@ -19,58 +15,48 @@ pub struct CliArgs {
 }
 
 pub fn process_cmdline() -> Result<CliArgs> {
-	let app = app();
+	let args = AppOptions::parse();
 
-	let arg_matches = app.get_matches();
-
-	if arg_matches.get_flag("bugreport") {
+	if args.bugreport {
 		bug_report::generate_bugreport();
 		std::process::exit(0);
 	}
-	if arg_matches.get_flag("logging") {
+	if args.logging {
 		setup_logging()?;
 	}
 
-	let workdir =
-		arg_matches.get_one::<String>("workdir").map(PathBuf::from);
-	let gitdir = arg_matches
-		.get_one::<String>("directory")
-		.map_or_else(|| PathBuf::from("."), PathBuf::from);
+	let gitdir = args.directory;
 
 	#[allow(clippy::option_if_let_else)]
-	let repo_path = if let Some(w) = workdir {
+	let repo_path = if let Some(w) = args.workdir {
 		RepoPath::Workdir { gitdir, workdir: w }
 	} else {
 		RepoPath::Path(gitdir)
 	};
 
-	let arg_theme = arg_matches
-		.get_one::<String>("theme")
-		.map_or_else(|| PathBuf::from("theme.ron"), PathBuf::from);
+	let cfg_path = get_app_config_path()?;
 
-	let theme = if get_app_config_path()?.join(&arg_theme).is_file() {
-		get_app_config_path()?.join(arg_theme)
-	} else {
-		get_app_config_path()?.join("theme.ron")
-	};
-
-	let notify_watcher: bool =
-		*arg_matches.get_one("watcher").unwrap_or(&false);
+	let theme = args
+		.theme
+		.and_then(|arg_theme| {
+			let arg_file = cfg_path.join(arg_theme);
+			arg_file.is_file().then_some(arg_file)
+		})
+		.unwrap_or_else(|| cfg_path.join("theme.ron"));
 
 	Ok(CliArgs {
 		theme,
 		repo_path,
-		notify_watcher,
+		notify_watcher: args.watcher,
 	})
 }
 
-fn app() -> ClapApp {
-	ClapApp::new(crate_name!())
-		.author(crate_authors!())
-		.version(crate_version!())
-		.about(crate_description!())
-		.help_template(
-			"\
+#[derive(Parser)]
+#[command(
+	author,
+	version,
+	about,
+	help_template = "\
 {before-help}gitui {version}
 {author}
 {about}
@@ -78,51 +64,33 @@ fn app() -> ClapApp {
 {usage-heading} {usage}
 
 {all-args}{after-help}
-		",
-		)
-		.arg(
-			Arg::new("theme")
-				.help("Set the color theme (defaults to theme.ron)")
-				.short('t')
-				.long("theme")
-				.value_name("THEME")
-				.num_args(1),
-		)
-		.arg(
-			Arg::new("logging")
-				.help("Stores logging output into a cache directory")
-				.short('l')
-				.long("logging")
-				.num_args(0),
-		)
-		.arg(
-			Arg::new("watcher")
-				.help("Use notify-based file system watcher instead of tick-based update. This is more performant, but can cause issues on some platforms. See https://github.com/extrawurst/gitui/blob/master/FAQ.md#watcher for details.")
-				.long("watcher")
-				.action(clap::ArgAction::SetTrue),
-		)
-		.arg(
-			Arg::new("bugreport")
-				.help("Generate a bug report")
-				.long("bugreport")
-				.action(clap::ArgAction::SetTrue),
-		)
-		.arg(
-			Arg::new("directory")
-				.help("Set the git directory")
-				.short('d')
-				.long("directory")
-				.env("GIT_DIR")
-				.num_args(1),
-		)
-		.arg(
-			Arg::new("workdir")
-				.help("Set the working directory")
-				.short('w')
-				.long("workdir")
-				.env("GIT_WORK_TREE")
-				.num_args(1),
-		)
+		"
+)]
+struct AppOptions {
+	/// Set the color theme (defaults to theme.ron)
+	#[arg(short = 't', long, value_name = "THEME")]
+	theme: Option<String>,
+
+	/// Stores logging output into a cache directory
+	#[arg(short = 'l', long)]
+	logging: bool,
+
+	/// Use notify-based file system watcher instead of tick-based update.
+	/// This is more performant, but can cause issues on some platforms. See https://github.com/extrawurst/gitui/blob/master/FAQ.md#watcher for details.
+	#[arg(long)]
+	watcher: bool,
+
+	/// Generate a bug report
+	#[arg(long)]
+	bugreport: bool,
+
+	/// Set the git directory
+	#[arg(short = 'd', long, default_value = ".", env = "GIT_DIR")]
+	directory: PathBuf,
+
+	/// Set the working directory
+	#[arg(short = 'w', long, env = "GIT_WORK_TREE")]
+	workdir: Option<PathBuf>,
 }
 
 fn setup_logging() -> Result<()> {
@@ -160,9 +128,4 @@ pub fn get_app_config_path() -> Result<PathBuf> {
 	path.push("gitui");
 	fs::create_dir_all(&path)?;
 	Ok(path)
-}
-
-#[test]
-fn verify_app() {
-	app().debug_assert();
 }
