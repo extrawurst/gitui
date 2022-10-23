0b693fc7a89cfac9945f2fd56381062645cec481
heiskane <henipe@gmail.com>
2022-10-23 18:57:25 +0300

    log worktree branchnames

--- asyncgit/src/sync/worktree.rs
+++ asyncgit/src/sync/worktree.rs
@@ -1,4 +1,4 @@
-use crate::sync::repository::repo;
+use crate::sync::{repository::{repo}, branch::get_branch_name};
 use scopetime::scope_time;
 use crate::error::Result;
 
@@ -17,13 +17,21 @@ pub fn worktrees(
 ) -> Result<Vec<WorkTree>> {
 	scope_time!("worktrees");
 
-	let repo = repo(repo_path)?;
+	let repo_obj = repo(repo_path)?;
 
-    Ok(repo.worktrees()?
+    let result: Vec<WorkTree> = repo_obj.worktrees()?
        .iter()
        .map(|s| WorkTree {
            name: s.unwrap().to_string()
        })
-       .collect()
-    )
+       .collect();
+
+    for r in result.iter() {
+        let worktree = repo_obj.find_worktree(&r.name)?;
+        let worktree_path = RepoPath::from(worktree.path().to_str().unwrap());
+        log::trace!("repo branch: {}", get_branch_name(&worktree_path)?);
+        log::trace!("worktree path: {:?}", worktree.path());
+    }
+
+    Ok(result)
 }
