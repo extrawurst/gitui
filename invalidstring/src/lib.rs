c705712a050584c593bc512eb30512d4470791a8
extrawurst <mail@rusticorn.com>
2022-10-26 14:34:33 +0200

    fix rust clippy

--- invalidstring/src/lib.rs
+++ invalidstring/src/lib.rs
@@ -3,6 +3,6 @@ pub fn invalid_utf8(prefix: &str) -> String {
 	let bytes = b"\xc3\x73";
 
 	unsafe {
-		format!("{}{}", prefix, std::str::from_utf8_unchecked(bytes))
+		format!("{prefix}{}", std::str::from_utf8_unchecked(bytes))
 	}
 }
