3559ffbce9a9add64db30796a183e194b87931cf
pm100 <paulmoore100@hotmail.com>
2023-04-22 13:05:02 -0700

    remove allow multiple

--- asyncgit/src/lib.rs
+++ asyncgit/src/lib.rs
@@ -23,7 +23,8 @@
 // #![deny(clippy::expect_used)]
 //TODO: consider cleaning some up and allow specific places
 #![allow(clippy::significant_drop_tightening)]
-#![allow(clippy::multiple_crate_versions)]
+
+//#![allow(clippy::multiple_crate_versions)]
 
 pub mod asyncjob;
 mod blame;
