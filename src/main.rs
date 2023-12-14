ca86f621da4d54813415e9b63ff0f08ed8bd18a3
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-12-13 23:59:47 -0500

    main.rs:TODO: prompt to generate nostr privkey

--- src/main.rs
+++ src/main.rs
@@ -132,6 +132,9 @@ fn main() -> Result<()> {
 		bail!("invalid path\nplease run gnostr-tui inside of a non-bare git repository");
 	}
 
+  //TODO: prompt to generate nostr privkey
+  //TODO: and add to git config
+
 	let key_config = KeyConfig::init()
 		.map_err(|e| eprintln!("KeyConfig loading error: {e}"))
 		.unwrap_or_default();
