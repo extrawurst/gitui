2c3c0ac8348ae17db56ecf621957620b6b0f7e2e
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-12-16 14:12:49 -0500

    make:cargo-i-gnostr-legit

---
+++ cargo.mk
@@ -0,0 +1,39 @@
+##
+##make cargo-*
+cargo-help:### 	cargo-help
+	@awk 'BEGIN {FS = ":.*?###"} /^[a-zA-Z_-]+:.*?###/ {printf "\033[36m%-15s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
+
+cargo-bt:cargo-build-tokio
+cargo-build-tokio:
+## 	make cargo-build-tokio q=true
+	@RUST_BACKTRACE=all $(CARGO) b $(QUIET) --no-default-features --features tokio
+
+cargo-bas:cargo-build-async-std### 	cargo-bas
+cargo-build-async-std:### 	cargo-build-async-std
+## 	make cargo-build-async-std q=true
+	@RUST_BACKTRACE=all $(CARGO) b $(QUIET) --no-default-features --features async-std
+
+cargo-install:### 	cargo install --path .
+#
+	#@$(CARGO) install --path $(PWD)
+	@$(CARGO) install --locked --path $(PWD)
+
+cargo-i-gnostr-legit:cargo-install-gnostr-legit### 	cargo-i-gnostr-legit
+cargo-install-gnostr-legit:
+	@$(CARGO) install --bins $(QUIET) --path ./legit
+cargo-bench:### 	cargo-bench
+## $(CARGO) bench
+	@$(CARGO) bench
+
+cargo-examples:### 	cargo-examples
+## $(CARGO) b --examples
+	@$(CARGO) b --examples
+
+cargo-report:### 	cargo-report
+	$(CARGO) report future-incompatibilities --id 1
+
+cargo-doc:### 	cargo-doc
+	 $(CARGO) doc --no-deps #--open
+
+# vim: set noexpandtab:
+# vim: set setfiletype make
---
+++ docker.mk
@@ -0,0 +1,39 @@
+## detect ARCH for buildx
+ARCH                                   :=$(shell uname -m)
+export ARCH
+ifeq ($(ARCH),x86_64)
+TARGET                                 :=amd64
+export TARGET
+endif
+ifeq ($(ARCH),arm64)
+TARGET                                 :=arm64
+export TARGET
+endif
+
+DOCKER=$(shell which docker)
+export DOCKER
+PWD=$(shell echo `pwd`)
+export PWD
+
+default:
+	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?##/ {printf "\033[36m%-15s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
+
+dockerx:docker-buildx## 	docker-buildx
+docker-build:## 	ocker build -f Dockerfile -t gnostr-command .
+	@./gnostr-command-docker -df start
+	@$(DOCKER) pull ghcr.io/gnostr-org/gnostr-command:latest
+	@$(DOCKER) build -f Dockerfile -t gnostr-command .
+docker-buildx:## 	docker buildx build sequence
+	@./gnostr-command-docker -df start
+	@$(DOCKER) run --privileged --rm tonistiigi/binfmt --install all
+	@$(DOCKER) buildx ls
+	@$(DOCKER) buildx create --use --name gnostr-command-buildx || true
+	@$(DOCKER) buildx build -t gnostr-command --platform linux/arm64,linux/amd64 .
+	@$(DOCKER) buildx build -t gnostr-command --platform linux/$(TARGET) . --load
+
+docker-package-buildx:
+	@docker build . --tag ghcr.io/gnostr-org/gnostr-command:latest
+docker-package-pushx:
+	@$(DOCKER) push ghcr.io/gnostr-org/gnostr-command:latest
+docker-rust:
+	@$(DOCKER) run --rm --user "$(shell id -u)":"$(shell id -g)" -v "$(PWD)":/usr/src/myapp -w /usr/src/myapp rust:1.74.0 cargo build --release --no-default-features --features async-std
