dca3f14b483447146739c0910055b2250ebf57ad
Christoph Rüßler <christoph.ruessler@mailbox.org>
2024-11-01 11:08:50 +0100

    Sleep longer
    This will hopefully make the test green on CI.

--- src/main.rs
+++ src/main.rs
@@ -470,7 +470,7 @@ mod tests {
 			AsyncNotification::Git(AsyncGitNotification::Status);
 		app.update_async(event).unwrap();
 
-		sleep(Duration::from_millis(20));
+		sleep(Duration::from_millis(500));
 
 		draw(&mut terminal, &app).unwrap();
 
@@ -485,7 +485,7 @@ mod tests {
 		))
 		.unwrap();
 
-		sleep(Duration::from_millis(20));
+		sleep(Duration::from_millis(500));
 
 		app.update().unwrap();
 
