2d7cc74576c579d5f384c6adc71506f1dc70b471
tk <49250442+tkr-sh@users.noreply.github.com>
2025-01-15 23:03:59 +0000

    🔊 debug: add logs

--- src/app.rs
+++ src/app.rs
@@ -289,7 +289,6 @@ impl App {
 
 	///
 	pub fn event(&mut self, ev: InputEvent) -> Result<()> {
-		//println!("event: {:?}", ev);
 		log::trace!("event: {:?}", ev);
 
 		if let InputEvent::Input(ev) = ev {
